5d18b6df15abb69754192cce15329411503a61c7
Tyler Gasperlin <gasperlint@gmail.com>
2020-02-08 11:22:39 -0800

    add reducer type checking

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,5 +1,16 @@
 import {combineReducers} from 'redux'
+import { todosReducer } from './todos'
+import {Todo} from '../actions'
 
-export const reducers = combineReducers({
-    counter: () => 1
+//describes state within redux store
+export interface StoreState {
+    todos: Todo[]
+}
+
+//redux looks at object we pass into combine reducers 
+export const reducers = combineReducers<StoreState>({
+    //typescript makes sure that we return a value of type array of Todos
+    //We want storestate to return an array of Todos
+    //so now if the todosReducer returns a number instead of an array of Todo we get a type error here
+    todos: todosReducer
 });
