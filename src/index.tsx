166ced820e6455d8ab6d414cf8711d1bfb28a7db
Tyler Gasperlin <gasperlint@gmail.com>
2020-02-08 10:20:01 -0800

    cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -26,23 +26,5 @@ const App = (props: AppProps): JSX.Element => {
     ) 
 }
 
-// class App extends React.Component<AppProps> {
-
-//     state = {counter: 0}
-
-//     onIncrement = (): void => {
-//         this.setState({counter: this.state.counter + 1})
-//     }
-
-//     onDecrement = (): void => {
-//         this.setState({counter: this.state.counter - 1})
-//     }
-
-//     render() {
-//         return(
-
-//         )
-//     }
-// }
 
 ReactDOM.render(<App color='red' />, document.querySelector('#root'));
