7b5810db283567a6f6c04de21045542205f0d0ec
Tyler Gasperlin <gasperlint@gmail.com>
2020-02-08 09:59:43 -0800

    onclick handlers

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-  render() {
-    return <div>Hi there</div>;
-  }
+interface AppProps {
+    color?: string;
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+
+class App extends React.Component<AppProps> {
+    state = {counter: 0};
+    onIncrement = (): void => {
+        this.setState({counter: this.state.counter + 1})
+    }
+
+    onDecrement = (): void => {
+        this.setState({counter: this.state.counter - 1})
+    }
+    
+    render() {
+        return(
+            <div>
+                <button onClick={this.onIncrement}>Increment</button>
+                <button onClick={this.onDecrement}>Decrement</button>
+                {this.state.counter}
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App color='red' />, document.querySelector('#root'));
