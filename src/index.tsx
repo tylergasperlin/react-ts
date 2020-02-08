3409015b4c4c9b8d002994cec845b661c2c53e66
Tyler Gasperlin <gasperlint@gmail.com>
2020-02-08 10:19:33 -0800

    functional component

--- src/index.tsx
+++ src/index.tsx
@@ -5,27 +5,44 @@ interface AppProps {
     color?: string;
 }
 
-class App extends React.Component<AppProps> {
 
-    state = {counter: 0}
+const App = (props: AppProps): JSX.Element => {
+    let [counter, setCounter] = React.useState(0)
+
+    const onIncrement = (): void => {
+        setCounter( counter + 1)
+    }
     
-    onIncrement = (): void => {
-        this.setState({counter: this.state.counter + 1})
+    const onDecrement = (): void => {
+        setCounter(counter - 1)
     }
-
-    onDecrement = (): void => {
-        this.setState({counter: this.state.counter - 1})
-    }
-
-    render() {
-        return(
-            <div>
-                <button onClick={this.onIncrement}>Increment</button>
-                <button onClick={this.onDecrement}>Decrement</button>
-                {this.state.counter}
-            </div>
-        )
-    }
+    
+    return (
+        <div>
+            <button onClick={onIncrement}>Increment</button>
+            <button onClick={onDecrement}>Decrement</button>
+            {counter}
+        </div>
+    ) 
 }
+
+// class App extends React.Component<AppProps> {
+
+//     state = {counter: 0}
+
+//     onIncrement = (): void => {
+//         this.setState({counter: this.state.counter + 1})
+//     }
+
+//     onDecrement = (): void => {
+//         this.setState({counter: this.state.counter - 1})
+//     }
+
+//     render() {
+//         return(
+
+//         )
+//     }
+// }
 
 ReactDOM.render(<App color='red' />, document.querySelector('#root'));
